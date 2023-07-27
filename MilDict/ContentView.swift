f2a1b77522fc1300bfa4e9e0c8b52c29d5503e3a
pobedria <pobedrik@gmail.com>
2023-07-27 15:46:38 +0300

    Add app icon. Divided logic into tabs

--- MilDict/ContentView.swift
+++ MilDict/ContentView.swift
@@ -9,15 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        TabView {
+            TerminologyView()
+                .tabItem {
+                    Label("Термінологія", systemImage: "text.book.closed")
+                }
+
+            AbbriviatinView()
+                .tabItem {
+                    Label("Абревіатури", systemImage: "character.bubble")
+                }
+        }
+    }
+}
+
+struct TerminologyView: View {
+    @State private var searchfield: String = ""
+    var body: some View {
+        VStack{
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("Hello, Vika")
+            Spacer()
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $searchfield)
+        }.padding()
     }
 }
+
+
+struct AbbriviatinView: View {
+    @State private var searchfield: String = ""
+    var body: some View {
+        VStack{
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Vikaa")
+            Spacer()
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $searchfield)
+        }.padding()
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
