d522064cf500756a61dc4075fdd4bba1329706db
pobedria <pobedrik@gmail.com>
2023-07-27 08:59:15 +0300

    Initial Commit

---
+++ MilDict/MilDictApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilDictApp.swift
+//  MilDict
+//
+//  Created by Viktor Pobedria on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MilDictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
