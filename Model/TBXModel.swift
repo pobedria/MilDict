fe7c6c0731b2c8b6fdc2d9e720ef8e070ce06988
pobedria <pobedrik@gmail.com>
2024-05-07 14:50:07 +0300

    Added TBXmodel

--- Model/TBXModel.swift
+++ Model/TBXModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+import Foundation
+
+struct TBXConcept: Codable, Identifiable {
+    enum CodingKeys: CodingKey {
+        case id
+        case en_title
+        case en_text
+        case ua_title
+        case ua_text
+    }
+    
+    var id: Int
+    var en_title: String
+    var en_text: String
+    var ua_title: String
+    var ua_text: String
+    
+}
+
+
+struct Descrip: Codable, Identifiable {
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case _type
+        case __text
+    }
+    var id: Int
+    var _type: String
+    var __text: String
+}
