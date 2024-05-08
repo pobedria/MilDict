94678b41f90529f8f2cc85e38f4160d70073e6cd
pobedria <pobedrik@gmail.com>
2024-05-08 10:49:59 +0300

    Implemented TBX decoder

--- Model/TBXModel.swift
+++ Model/TBXModel.swift
@@ -10,21 +10,55 @@ import Foundation
 import Foundation
 
 struct TBXConcept: Decodable{
-    enum CodingKeys: CodingKey {
-        case _id, descrip
-    }
-    
-    var _id: String
-    var descrip: Descrip
+    let _id: String
+    let descrip: Descrip
+    let langSec: [LangElement]
 }
 
 
-struct Descrip: Codable{
-    enum CodingKeys: CodingKey {
+struct Descrip: Decodable {
+    let _type: String
+    let __text: String
+}
 
-        case _type
-        case __text
+
+struct LangElement: Decodable{
+    let termSec: Any
+    let lang: String
+    
+    enum CodingKeys: String, CodingKey {
+        case termSec
+        case lang  = "_xml:lang"
     }
-    var _type: String
-    var __text: String
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+
+        
+        lang = try values.decode(String.self, forKey: .lang)
+        
+        if let str = try? values.decode(TermSecElement.self, forKey: .termSec) {
+            termSec = str
+        } else if let content = try? values.decode([TermSecElement].self, forKey: .termSec) {
+            termSec = content
+        } else {
+            throw DecodingError.dataCorruptedError(forKey:.termSec, in: values, debugDescription: "Value cannot be decoded!")
+        }
+    }
+}
+
+struct TermSecElement: Decodable {
+    let term: TBXTerm
+    let descrip: Descrip?
+    let xref: TBXxref?
+}
+
+struct TBXTerm: Decodable{
+    let _id: String
+    let __text: String
+}
+
+struct TBXxref: Decodable {
+    let _type: String
+    let _target: String
 }
