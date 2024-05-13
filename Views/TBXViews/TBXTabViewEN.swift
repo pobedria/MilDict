73569f4d62320941242732fbedc22c5410d953d3
pobedria <pobedrik@gmail.com>
2024-05-13 09:58:52 +0300

    Separated into 2 languages

---
+++ Views/TBXViews/TBXTabViewEN.swift
@@ -0,0 +1,44 @@
+//
+//  TBXTabView.swift
+//  MilDict
+//
+//  Created by Viktor Pobedria on 10.05.2024.
+//
+
+import SwiftUI
+
+struct TBXTabViewEN: View {
+    @State private var searchField: String = ""
+    var terms = loadAppTerms()
+    
+    var body: some View {
+        VStack() {
+            let sanitizedField = searchField
+                .lowercased()
+                .trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            NavigationView {
+                if sanitizedField.isEmpty{
+                    let words = terms.filter({ $0.lang == "en"})
+                    TBXListView(terms: words)
+                }
+                else {
+                    let filtered = terms.filter(
+                        where:{ $0.term.lowercased().contains(sanitizedField)},
+                        limit: 20)
+                    TBXListView(terms: filtered)
+                }
+            }
+            .navigationTitle("UK")
+            TextField("Пошук", text: $searchField,prompt:  Text("Пошук").foregroundColor(Color("LightKhaki")))
+                .background(Color("Olive"))
+                .textFieldStyle(MDTextFieldStyle())
+                .foregroundColor(Color("Gold"))
+            Spacer()
+        }.background(Color("Olive"))
+    }
+}
+
+#Preview {
+    TBXTabViewEN()
+}
--- Views/TBXViews/TBXTabViewUA.swift
+++ Views/TBXViews/TBXTabViewUA.swift
@@ -1,41 +1,15 @@
 //
-//  TBXTabView.swift
+//  TBXTabViewUA.swift
 //  MilDict
 //
-//  Created by Viktor Pobedria on 10.05.2024.
+//  Created by Viktor Pobedria on 13.05.2024.
 //
 
 import SwiftUI
 
 struct TBXTabViewUA: View {
-    @State private var searchField: String = ""
-    var terms = loadAppTerms()
-    
     var body: some View {
-        VStack() {
-            let sanitizedField = searchField
-                .lowercased()
-                .trimmingCharacters(in: .whitespacesAndNewlines)
-            
-            NavigationView {
-                if sanitizedField.isEmpty{
-                    let words = terms
-                    TBXListView(terms: words)
-                }
-                else {
-                    let filtered = terms.filter(
-                        where:{ $0.term.lowercased().contains(sanitizedField)},
-                        limit: 20)
-                    TBXListView(terms: filtered)
-                }
-            }
-            .navigationTitle("UK")
-            TextField("Пошук", text: $searchField,prompt:  Text("Пошук").foregroundColor(Color("LightKhaki")))
-                .background(Color("Olive"))
-                .textFieldStyle(MDTextFieldStyle())
-                .foregroundColor(Color("Gold"))
-            Spacer()
-        }.background(Color("Olive"))
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
