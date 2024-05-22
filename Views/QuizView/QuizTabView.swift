283872b6674dfa778e9ed367e613b481105477b3
pobedria <pobedrik@gmail.com>
2024-05-22 11:49:49 +0300

    Fixed bugs in quiz

--- Views/QuizView/QuizTabView.swift
+++ Views/QuizView/QuizTabView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct QuizTabView: View {
     @State private var concepts = TermsSorage.allConcepts.choose(4)
     @State private var correctNumber: Int = Int.random(in: 0..<4)
-    @State private var selection: String?
+    @State private var selection: Int?
+    @State private var blockedButtons = false
     
     var body: some View {
 
@@ -26,10 +27,10 @@ struct QuizTabView: View {
                 .foregroundColor(Color("Gold"))
                 .background(Color("Olive"))
             if let selection {
-                if options.filter({ $0.name == selection }).first!.isCorrect{
+                if selection == correctNumber{
                     Text ("Вірно")
                         .foregroundColor(.green)
-                        .font(Font.custom("UAFSans-Medium", size: 30))
+                        .font(Font.custom("UAFSans-Bold", size: 30))
                 } else {
                     Text ("Правильна відповідь:\n\(options[correctNumber].name)")
                         .foregroundColor(.red)
@@ -37,17 +38,24 @@ struct QuizTabView: View {
                 }
                 
             }
-            List(options, id: \.name) { option in
+            ForEach(Array(zip(options.indices, options)), id: \.0) { index, option in
                 Button(option.name){
-                    selection = option.name 
-                    withAnimation(.easeInOut(duration: 5)) {
-                        correctNumber = Int.random(in: 0..<4)
-                        selection = nil
-                        concepts = TermsSorage.allConcepts.choose(4)
+                    if !blockedButtons {
+                        blockedButtons = true
+                        selection = index
+                        Task {
+                            await delayPageUpdate()
+                        }
                     }
-                }
+                    
+                }.frame(maxWidth: .infinity)
+                .padding()
+                .background(Color("Steppe"))
+                .clipShape(Capsule())
                 
             }.scrollContentBackground(.hidden)
+                .padding(.horizontal)
+            
 
         }
         .padding(.bottom)
@@ -56,6 +64,19 @@ struct QuizTabView: View {
         .frame(maxWidth: .infinity,maxHeight: .infinity)
         .background(Color("Olive"))
         .font(Font.custom("UAFSans-Medium", size: 20))
+        
+    }
+    private func delayPageUpdate() async {
+        // Delay of 7.5 seconds (1 second = 1_000_000_000 nanoseconds)
+        sleep(2)
+        withAnimation(){
+            print("Block false")
+            blockedButtons = false
+            correctNumber = Int.random(in: 0..<4)
+            selection = nil
+            concepts = TermsSorage.allConcepts.choose(4)
+        }
+        
         
     }
 }
