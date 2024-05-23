e4c3700d39544e592a47f9921a395c48970886b2
pobedria <pobedrik@gmail.com>
2024-05-23 14:56:15 +0300

    Good iPad adoptation

--- Views/TBXViews/TBXDetailView.swift
+++ Views/TBXViews/TBXDetailView.swift
@@ -36,16 +36,7 @@ struct TBXDetailView: View {
 }
 
 #Preview {
-    let terms = [
-        AppTerm(id: 1, conceptId: 10, subject: "102 – стратегічні комунікації", lang: "en", term: "PsyOp"),
-        AppTerm(id: 2, conceptId: 10, subject: "102 – стратегічні комунікації", lang: "en", term: "psychological operation", description: "Planned activities using methods of communication and other means directed at approved audiences in order to influence perceptions, attitudes and behaviour, affecting the achievement of political and military objectives.", xref: "https://github.com/pobedria/mildictmeta/blob/main/AAP-06.pdf"),
-        AppTerm(id: 3, conceptId: 10, subject: "102 – стратегічні комунікації", lang: "uk", term: "інформаційно-психологічна операція"),
-        AppTerm(id: 4, conceptId: 10, subject: "102 – стратегічні комунікації", lang: "uk", term: "ІПСО"),
-        AppTerm(id: 5, conceptId: 20, subject: "500 – оборонне планування", lang: "en", term: "homeland defense"),
-        AppTerm(id: 6, conceptId: 33, subject: "602 – електромагнітна та кіберборотьба", lang: "en", term: "electronic warfare"),
-        AppTerm(id: 7, conceptId: 33, subject: "701 – навчально-бойові завдання", lang: "en", term: "OPFOR"),
-        AppTerm(id: 9, conceptId: 33, subject: "900 – цивільно-військове співробітництво", lang: "en", term: "humanitarian aid")
+    let term = AppTerm(id: 1, conceptId: 326712, subject: "102 – стратегічні комунікації", lang: "en", term: "PsyOp")
     
-    ]
-    return TBXDetailView(chosenTerm: terms[0])
+    return TBXDetailView(chosenTerm: term)
 }
--- Views/TBXViews/TBXListView.swift
+++ Views/TBXViews/TBXListView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct TBXListView: View {
-    
+    @State private var selectedTerm: AppTerm?
     let terms: [AppTerm]
 
     var body: some View {
@@ -25,46 +25,43 @@ struct TBXListView: View {
 //        }
 //        .foregroundColor(Color("Gold"))
         
-        NavigationStack {
-            List(terms) { term in
-   
-                NavigationLink(term.term, value: term)
-    
-                    .foregroundColor(Color("Gold"))
-                    .listRowBackground(Color("Olive"))
-                
-            }
-            .navigationDestination(for: AppTerm.self) { term in
-                TBXDetailView(chosenTerm: term)
+//        NavigationStack {
+//            List(terms) { term in
+//                
+//                NavigationLink {
+//                    TBXDetailView(chosenTerm: term)
+//                } label: {
+//                    TBXPreView(term: term)
+//                }.listRowBackground(Color("Olive"))
+//            }.background(Color("Olive"))
+//                .scrollContentBackground(.hidden)
+//        }
+        
+        NavigationSplitView {
+            List(terms, selection: $selectedTerm) { term in
+                NavigationLink {
+                    TBXDetailView(chosenTerm: term)
+                } label: {
+                    TBXPreView(term: term)
+                }.listRowBackground(Color("Olive"))
             }.background(Color("Olive"))
                 .scrollContentBackground(.hidden)
-        }
-        
-//        NavigationSplitView {
-//            List(terms, selection: $selectedTerm) { term in
-//                NavigationLink(term.term, value: term)
-//                    .foregroundColor(Color("Gold"))
-//                    .listRowBackground(Color("Olive"))
-//            }
-//        } detail: {
-//            TBXDetailView(chosenTerm: selectedTerm ?? terms[0])
-//        }.navigationTitle("Terms")
-//            .background(Color("Olive"))
-//            .scrollContentBackground(.hidden)
-//        
+        } detail: {
+            TBXDetailView(chosenTerm: selectedTerm ?? terms[0])
+        }.navigationTitle("Terms")
     }
 }
 
 #Preview {
     let terms = [
-        AppTerm(id: 1, conceptId: 33, subject: "104 – військова кадрова політика", lang: "en", term: "PsyOp"),
-        AppTerm(id: 2, conceptId: 33, subject: "202 – зовнішня розвідка", lang: "en", term: "agent"),
-        AppTerm(id: 3, conceptId: 33, subject: "300 – оперативне управління (штабні процедури)", lang: "en", term: "C2"),
-        AppTerm(id: 4, conceptId: 33, subject: "432 – боєприпаси та вибухові речовини", lang: "en", term: "demolition system"),
-        AppTerm(id: 5, conceptId: 33, subject: "500 – оборонне планування", lang: "en", term: "homeland defense"),
-        AppTerm(id: 6, conceptId: 33, subject: "602 – електромагнітна та кіберборотьба", lang: "en", term: "electronic warfare"),
-        AppTerm(id: 7, conceptId: 33, subject: "701 – навчально-бойові завдання", lang: "en", term: "OPFOR"),
-        AppTerm(id: 9, conceptId: 33, subject: "900 – цивільно-військове співробітництво", lang: "en", term: "humanitarian aid")
+        AppTerm(id: 1, conceptId: 326712, subject: "104 – військова кадрова політика", lang: "en", term: "PsyOp"),
+        AppTerm(id: 2, conceptId: 326080, subject: "202 – зовнішня розвідка", lang: "en", term: "agent"),
+        AppTerm(id: 3, conceptId: 330696, subject: "300 – оперативне управління (штабні процедури)", lang: "en", term: "C2"),
+        AppTerm(id: 4, conceptId: 329900, subject: "432 – боєприпаси та вибухові речовини", lang: "en", term: "demolition system"),
+        AppTerm(id: 5, conceptId: 330690, subject: "500 – оборонне планування", lang: "en", term: "homeland defense"),
+        AppTerm(id: 6, conceptId: 326718, subject: "602 – електромагнітна та кіберборотьба", lang: "en", term: "electronic warfare"),
+        AppTerm(id: 7, conceptId: 326944, subject: "701 – навчально-бойові завдання", lang: "en", term: "OPFOR"),
+        AppTerm(id: 9, conceptId: 326262, subject: "900 – цивільно-військове співробітництво", lang: "en", term: "humanitarian aid")
     
     ]
     return TBXListView(terms: terms)
