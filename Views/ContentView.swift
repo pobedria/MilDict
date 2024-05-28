07b28d479aac00eefc0419a475e17297b6c5d217
pobedria <pobedrik@gmail.com>
2024-05-28 15:39:24 +0300

    Header's shadow

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -12,7 +12,6 @@ struct ContentView: View {
     var body: some View {
         VStack{
             AppHeader()
-            
             TabView {
                 TBXTabView(lang: "en")
                     .tabItem {
--- Views/TBXViews/AppHeader.swift
+++ Views/TBXViews/AppHeader.swift
@@ -9,20 +9,26 @@ import SwiftUI
 
 struct AppHeader: View {
     var body: some View {
-        HStack{
-            
-            Image("VIKNU")
-                .resizable()
-                .frame(width: 40.0, height: 40.0)
-
-            Text("MilDict")
-                .font(Font.custom("UAFSans-Regular", size: 25))
-                .foregroundColor(.white)
-            Spacer()
-            Image("LRDD")
-                .resizable()
-                .frame(width: 80.0, height: 40.0)
-        }.padding(.horizontal)
+        ZStack{
+            Rectangle()
+                .fill(Color("Olive"))
+                .frame(width: UIScreen.main.bounds.width, height: 60)
+                .shadow( radius: 2,  x: 0, y:3)
+            HStack{
+                
+                Image("VIKNU")
+                    .resizable()
+                    .frame(width: 40.0, height: 40.0)
+                
+                Text("MilDict")
+                    .font(Font.custom("Volja-Regular", size: 20))
+                    .foregroundColor(Color("Steppe"))
+                Spacer()
+                Image("LRDD")
+                    .resizable()
+                    .frame(width: 80.0, height: 40.0)
+            }.padding(.horizontal)
+        }
     }
 }
 
