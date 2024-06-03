4af38509401eca78d0311107c74446c032c71560
pobedria <pobedrik@gmail.com>
2024-06-03 10:20:14 +0300

    Full search instead of limited

--- Helpers/extensions.swift
+++ Helpers/extensions.swift
@@ -8,84 +8,84 @@
 import Foundation
 import SwiftUI
 
-extension String {
-    var isLatin: Bool {
-        let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        let lower = "abcdefghijklmnopqrstuvwxyz"
+//extension String {
+//    var isLatin: Bool {
+//        let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//        let lower = "abcdefghijklmnopqrstuvwxyz"
+//
+//        for c in self.map({ String($0) }) {
+//            if !upper.contains(c) && !lower.contains(c) {
+//                return false
+//            }
+//        }
+//
+//        return true
+//    }
+//
+//    var isCyrillic: Bool {
+//        let upper = "АБВГҐДЕЖЗИЙІЇКЛМНОПРСТУФХЦЧШЩЬЮЯ"
+//        let lower = "абвгґдежзийіїклмнопрстуфхцчшщьюя"
+//
+//        for c in self.map({ String($0) }) {
+//            if !upper.contains(c) && !lower.contains(c) {
+//                return false
+//            }
+//        }
+//
+//        return true
+//    }
+//
+//    var isBothLatinAndCyrillic: Bool {
+//        return self.isLatin && self.isCyrillic
+//    }
+//}
 
-        for c in self.map({ String($0) }) {
-            if !upper.contains(c) && !lower.contains(c) {
-                return false
-            }
-        }
+//extension Sequence {
+//    public func filter(where isIncluded: (Iterator.Element) -> Bool, limit: Int) -> [Iterator.Element] {
+//        var result : [Iterator.Element] = []
+//        result.reserveCapacity(limit)
+//        var count = 0
+//        var it = makeIterator()
+//
+//        // While limit not reached and there are more elements ...
+//        while count < limit, let element = it.next() {
+//            if isIncluded(element) {
+//                result.append(element)
+//                count += 1
+//            }
+//        }
+//        return result
+//    }
+//}
 
-        return true
-    }
+//extension View {
+//    func placeholder<Content: View>(
+//        when shouldShow: Bool,
+//        alignment: Alignment = .leading,
+//        @ViewBuilder placeholder: () -> Content) -> some View {
+//
+//        ZStack(alignment: alignment) {
+//            placeholder().opacity(shouldShow ? 1 : 0)
+//            self
+//        }
+//    }
+//}
 
-    var isCyrillic: Bool {
-        let upper = "АБВГҐДЕЖЗИЙІЇКЛМНОПРСТУФХЦЧШЩЬЮЯ"
-        let lower = "абвгґдежзийіїклмнопрстуфхцчшщьюя"
-
-        for c in self.map({ String($0) }) {
-            if !upper.contains(c) && !lower.contains(c) {
-                return false
-            }
-        }
-
-        return true
-    }
-
-    var isBothLatinAndCyrillic: Bool {
-        return self.isLatin && self.isCyrillic
-    }
-}
-
-extension Sequence {
-    public func filter(where isIncluded: (Iterator.Element) -> Bool, limit: Int) -> [Iterator.Element] {
-        var result : [Iterator.Element] = []
-        result.reserveCapacity(limit)
-        var count = 0
-        var it = makeIterator()
-
-        // While limit not reached and there are more elements ...
-        while count < limit, let element = it.next() {
-            if isIncluded(element) {
-                result.append(element)
-                count += 1
-            }
-        }
-        return result
-    }
-}
-
-extension View {
-    func placeholder<Content: View>(
-        when shouldShow: Bool,
-        alignment: Alignment = .leading,
-        @ViewBuilder placeholder: () -> Content) -> some View {
-
-        ZStack(alignment: alignment) {
-            placeholder().opacity(shouldShow ? 1 : 0)
-            self
-        }
-    }
-}
-
-extension String {
-    func compareDigitsInTheEnd(_ other: String) -> Bool{
-        if let leftChar = self.first, let rightChar = other.first {
-            if !leftChar.isLetter && rightChar.isLetter {
-                return false
-            } else if leftChar.isLetter && !rightChar.isLetter {
-                return true
-            } else {
-                return self < other
-            }
-        } else {
-            return self < other
-        }
-    }
-}
+//extension String {
+//    func compareDigitsInTheEnd(_ other: String) -> Bool{
+//        if let leftChar = self.first, let rightChar = other.first {
+//            if !leftChar.isLetter && rightChar.isLetter {
+//                return false
+//            } else if leftChar.isLetter && !rightChar.isLetter {
+//                return true
+//            } else {
+//                return self < other
+//            }
+//        } else {
+//            return self < other
+//        }
+//    }
+//}
 
 
 extension Collection {
--- Views/TBXViews/TBXListView.swift
+++ Views/TBXViews/TBXListView.swift
@@ -28,7 +28,7 @@ struct TBXListView: View {
             TBXDetailView(chosenTerm: selectedTerm ?? TermsSorage.enTerms[0])
         }
         .navigationBarColor(UIColor(Color("Olive")))
-        .searchable(text: $searchText, prompt: "Look for something")
+        .searchable(text: $searchText, prompt: "Пошук термінів")
     }
     
     var searchResults: [AppTerm] {
@@ -36,24 +36,11 @@ struct TBXListView: View {
             return lang == "en" ? TermsSorage.enTerms : TermsSorage.ukTerms
         } else {
             let sanitizedField = searchText.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-            return TermsSorage.allTerms.filter(
-                where:{ $0.term.lowercased().contains(sanitizedField)},
-                limit: 20).sorted()
+            return TermsSorage.allTerms.filter({ $0.term.lowercased().contains(sanitizedField)}).sorted()
         }
     }
 }
 
 #Preview {
-    let terms = [
-        AppTerm(id: 1, conceptId: 326712, subject: "104 – військова кадрова політика", lang: "en", term: "PsyOp"),
-        AppTerm(id: 2, conceptId: 326080, subject: "202 – зовнішня розвідка", lang: "en", term: "agent"),
-        AppTerm(id: 3, conceptId: 330696, subject: "300 – оперативне управління (штабні процедури)", lang: "en", term: "C2"),
-        AppTerm(id: 4, conceptId: 329900, subject: "432 – боєприпаси та вибухові речовини", lang: "en", term: "demolition system"),
-        AppTerm(id: 5, conceptId: 330690, subject: "500 – оборонне планування", lang: "en", term: "homeland defense"),
-        AppTerm(id: 6, conceptId: 326718, subject: "602 – електромагнітна та кіберборотьба", lang: "en", term: "electronic warfare"),
-        AppTerm(id: 7, conceptId: 326944, subject: "701 – навчально-бойові завдання", lang: "en", term: "OPFOR"),
-        AppTerm(id: 9, conceptId: 326262, subject: "900 – цивільно-військове співробітництво", lang: "en", term: "humanitarian aid")
-        
-    ]
     return TBXListView(lang: "en")
 }
